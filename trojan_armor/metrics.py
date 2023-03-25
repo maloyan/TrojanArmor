2a1371e2f4044ba175c797d9e4c4a464a58250cf
Narek Maloyan <narek1110@gmail.com>
2023-03-25 12:33:29 +0300

    metrics

--- trojan_armor/metrics.py
+++ trojan_armor/metrics.py
@@ -1,6 +1,6 @@
 # metrics.py
-
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
+import numpy as np
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_auc_score
 
 class Metrics:
     @staticmethod
@@ -23,3 +23,41 @@ class Metrics:
     def confusion_matrix(y_true, y_pred, labels=None):
         return confusion_matrix(y_true, y_pred, labels=labels)
 
+    @staticmethod
+    def clean_accuracy(y_true_clean, y_pred_clean):
+        return accuracy_score(y_true_clean, y_pred_clean)
+
+    @staticmethod
+    def attack_success_rate(y_true_poisoned, y_pred_poisoned, target_label):
+        successful_attacks = np.sum((y_pred_poisoned == target_label) & (y_true_poisoned != target_label))
+        total_attacks = np.sum(y_true_poisoned != target_label)
+        return successful_attacks / total_attacks
+
+    @staticmethod
+    def robust_accuracy(y_true_poisoned, y_pred_poisoned):
+        return accuracy_score(y_true_poisoned, y_pred_poisoned)
+
+    @staticmethod
+    def trojan_misclassification_confidence(y_pred_poisoned, target_label):
+        target_probs = y_pred_poisoned[y_pred_poisoned.argmax(axis=1) == target_label].max(axis=1)
+        return np.mean(target_probs)
+
+    @staticmethod
+    def clean_accuracy_drop(y_true_clean, y_pred_clean, y_true_clean_defended, y_pred_clean_defended):
+        clean_acc = Metrics.clean_accuracy(y_true_clean, y_pred_clean)
+        clean_acc_defended = Metrics.clean_accuracy(y_true_clean_defended, y_pred_clean_defended)
+        return clean_acc - clean_acc_defended
+
+    @staticmethod
+    def clean_classification_confidence(y_pred_clean):
+        return np.mean(y_pred_clean.max(axis=1))
+
+    @staticmethod
+    def efficacy_specificity_auc(y_true, y_pred_scores, pos_label):
+        return roc_auc_score(y_true, y_pred_scores)
+
+    @staticmethod
+    def neuron_separation_ratio():
+        # This metric is model-specific and depends on neuron activations.
+        # You need to provide more details on how to calculate it.
+        pass
