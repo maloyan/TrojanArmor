21d18c82fb39908e44ce7c35aee0141b7b84f46b
Narek Maloyan <narek1110@gmail.com>
2023-03-25 09:43:59 +0300

    trojan_armor/models.py

--- trojan_armor/models.py
+++ trojan_armor/models.py
@@ -1,9 +1,12 @@
 # models.py
 import timm
+import torch
 
 def get_model(model_name, num_classes):
     if model_name.startswith("timm_"):
         return get_timm_model(model_name[5:], num_classes)
+    elif model_name.startswith("hf_"):
+        return get_huggingface_model(model_name[3:])
     else:
         # Add other model sources here
         raise ValueError(f"Unsupported model: {model_name}")
@@ -18,3 +21,11 @@ def get_timm_model(model_name, num_classes):
     
     model = timm.create_model(model_name, pretrained=True, num_classes=num_classes)
     return model
+
+def get_huggingface_model(model_name):
+    try:
+        model = timm.create_model(f"hf_hub:{model_name}", pretrained=True)
+        return model
+    except Exception as e:
+        print(f"Error loading model '{model_name}' from Hugging Face: {e}")
+        raise
